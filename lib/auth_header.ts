31e8a1f8f60a598d2e03abae8bb2beb3890a9eb9
Arnaud "Baloran" Allouis <arnaud.allouis@unlikely.technology>
2025-01-26 22:05:06 +0100

    chore(all): WIP: update to vitest + typescript + begin rewrite code

---
+++ lib/auth_header.ts
@@ -0,0 +1,11 @@
+var re = /(\S+)\s+(\S+)/
+
+function parseAuthHeader(hdrValue) {
+  if (typeof hdrValue !== 'string') {
+    return null
+  }
+  var matches = hdrValue.match(re)
+  return matches && { scheme: matches[1], value: matches[2] }
+}
+
+export { parseAuthHeader }
